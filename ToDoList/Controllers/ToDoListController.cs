eb09b9e8d6f962067b902a9639d7c2abaea7d94c
psingh1028 <pawanjotsingh1997@gmail.com>
2020-12-04 20:27:26 -0500

    added form to create a task

--- ToDoList/Controllers/ToDoListController.cs
+++ ToDoList/Controllers/ToDoListController.cs
@@ -4,16 +4,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ToDoList.Models;
+using ToDoList.ViewModels;
 
 namespace ToDoList.Controllers
 {
     public class ToDoListController : Controller
     {
+        [HttpGet]
         public IActionResult ListOfItems()
         {
             TaskToDo task = new TaskToDo("clean", "Dec_3rd", "Dec_4th");
             return View(task);
 
+        }
+        public IActionResult CreateTask()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreateTask(CreateTaskViewModel viewModel)
+        {
+            return View();
         }
     }
 }
---
+++ ToDoList/ViewModels/CreateTaskViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoList.ViewModels
+{
+    public class CreateTaskViewModel
+    {
+        public string TaskName { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+    }
+}
