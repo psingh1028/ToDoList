37d9b024192007a6a1cc4a62b45dc7cc9384cfa7
psingh1028 <pawanjotsingh1997@gmail.com>
2020-12-04 19:55:15 -0500

    initial commit

---
+++ ToDoList/Controllers/ToDoListController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+    public class ToDoListController : Controller
+    {
+        public IActionResult ListOfItems()
+        {
+            TaskToDo task = new TaskToDo("clean", "Dec_3rd", "Dec_4th");
+            return View(task);
+
+        }
+    }
+}
---
+++ ToDoList/Models/TaskToDo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ToDoList.Models
+{
+    public class TaskToDo
+    {
+
+        public string TaskName { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+        public TaskToDo(string taskName, string startDate, string endDate)
+        {
+            TaskName = taskName;
+            StartDate = startDate;
+            EndDate = endDate;
+        }
+    }
+}
